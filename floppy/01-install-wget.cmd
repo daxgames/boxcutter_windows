4bb741ee7d16b474c0848733a147175996d47f3c
Dax T. Games <dax.games@bedbath.com>
2020-02-26 11:02:54 -0500

    sync

--- floppy/01-install-wget.cmd
+++ floppy/01-install-wget.cmd
@@ -14,25 +14,9 @@ copy /y "%~dp0\_download.cmd" "%SystemRoot%\"
 
 call "%~dp0\_download.cmd" "%WGET_URL%" "%filename%"
 
-if "%DISABLE_BITS%" == "1" if not exist "%filename%" goto exit1
-
-if exist "%filename%" goto exit0
-
-set bitsadmin=
-
-for %%i in (bitsadmin.exe) do set bitsadmin=%%~$PATH:i
-
 if defined DISABLE_BITS (
     if "%DISABLE_BITS%" == "1" if not exist "%filename%" goto exit1
 )
-
-if not exist "%bitsadmin%" goto exit1
-
-for %%i in ("%filename%") do set jobname=%%~nxi
-
-"%bitsadmin%" /transfer "%jobname%" "%WGET_URL%" "%filename%"
-
-if exist "%filename%" goto exit0
 
 goto exit1
 
